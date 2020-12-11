caa95b9bdce90604d93aeec948cc426bb2d8adf5
coderdeepsy <70744214+coderdeepsy@users.noreply.github.com>
2020-12-11 17:24:43 +0530

    Update Log.js

--- Log.js
+++ Log.js
@@ -5,7 +5,7 @@ class Log {
           'friction':0.3,
           'density':1.0
       }
-      this.body = Bodies.rectangle(x, y,20, height,angle, options);
+      this.body = Bodies.rectangle(x, y, height,angle, options);
       this.width = 300;
       this.height = height;
       Matter.Body.setAngle(this.body,angle)
